8a50ef8f419b37ac80330d6201099a159398af7a
Mike Samuel <mikesamuel@gmail.com>
2011-10-18 00:22:30 -0400

    added copyright headers

--- src/main/com/google/autoesc/ReplacementTable.java
+++ src/main/com/google/autoesc/ReplacementTable.java
@@ -1,3 +1,17 @@
+// Copyright (C) 2011 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.autoesc;
 
 import java.io.IOException;
@@ -6,6 +20,10 @@ import java.util.Arrays;
 
 import javax.annotation.Nullable;
 
+/**
+ * ReplacementTable maps strings in one language (e.g. plain text) to strings
+ * in another (e.g. HTML) by transforming the strings character by character.
+ */
 class ReplacementTable {
   private String[] table;
   private int[] nonAscii;
